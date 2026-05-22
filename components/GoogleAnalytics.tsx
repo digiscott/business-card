5e28d8fd516f79f25bd198e225fffb17d3010970
Scott Whitlock <scooter.whitlock@gmail.com>
2026-05-22 11:01:44 -0400

    feat: integrate Google Tag Manager for analytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -7,28 +7,26 @@ import { CookieConsentBanner } from "./CookieConsentBanner";
 declare global {
   interface Window {
     dataLayer: unknown[];
-    gtag: (...args: unknown[]) => void;
   }
 }
 
 const CONSENT_STORAGE_KEY = "analytics-consent";
 
 type Props = {
-  measurementId: string;
+  gtmId: string;
 };
 
 function updateConsent(consent: "granted" | "denied") {
-  if (typeof window.gtag === "function") {
-    window.gtag("consent", "update", {
-      analytics_storage: consent,
-      ad_storage: "denied",
-      ad_user_data: "denied",
-      ad_personalization: "denied",
+  if (window.dataLayer) {
+    window.dataLayer.push({
+      "gtm.start": new Date().getTime(),
+      event: "gtm.consent",
+      "consent.analytics_storage": consent,
     });
   }
 }
 
-export function GoogleAnalytics({ measurementId }: Props) {
+export function GoogleAnalytics({ gtmId }: Props) {
   useEffect(() => {
     const storedConsent = window.localStorage.getItem(CONSENT_STORAGE_KEY);
     if (storedConsent === "granted") {
@@ -38,22 +36,25 @@ export function GoogleAnalytics({ measurementId }: Props) {
 
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          window.gtag = gtag;
-          gtag('js', new Date());
-          gtag('consent', 'default', {
-            analytics_storage: 'denied',
-            ad_storage: 'denied',
-            ad_user_data: 'denied',
-            ad_personalization: 'denied'
-          });
-          gtag('config', '${measurementId}', { anonymize_ip: true });
-        `}
-      </Script>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`,
+        }}
+      />
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
       <CookieConsentBanner onAccept={() => updateConsent("granted")} onDecline={() => updateConsent("denied")} />
     </>
   );
